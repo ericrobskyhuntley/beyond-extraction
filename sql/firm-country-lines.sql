4e911fa9bfd87c720680e265e6c2cfe5e4652c1b
Eric Robsky Huntley <ehuntley@mit.edu>
2020-02-25 14:06:50 -0500

    revise sql

--- sql/firm-country-lines.sql
+++ sql/firm-country-lines.sql
@@ -1,6 +1,10 @@
-SELECT c.country, f.name, st_makeline(c.geom, f.geom)
-FROM countries AS c
-INNER JOIN fc
-	ON c.country=fc.country
-INNER JOIN firms AS f
-	ON fc.name=f.name;
+SELECT 
+	f.name AS firm, 
+	ST_UNION(ST_MAKELINE(f.geom, ST_CENTROID(c.geom))) AS geom
+FROM 
+	firms_ix AS f
+	INNER JOIN firms_ix_by_country AS fc
+		ON f.name=fc.name
+	INNER JOIN countries AS c
+		ON fc.country=c.country
+GROUP BY (f.name);
