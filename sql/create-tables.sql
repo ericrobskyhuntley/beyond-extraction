f515fdf3644a1c53616a7faa218cbaf6439f5ce9
Eric Robsky Huntley <ehuntley@mit.edu>
2020-02-27 14:58:08 -0500

    sql works from psql or run through query window

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,5 +1,5 @@
 -- Drop tables if they exist.
-DROP VIEW IF EXISTS exhibitor_addresses;
+
 DROP TABLE IF EXISTS ix_by_commodity;
 DROP TABLE IF EXISTS ix_by_country;
 DROP TABLE IF EXISTS ts_by_biztype;
@@ -10,6 +10,7 @@ DROP TABLE IF EXISTS booths;
 DROP TABLE IF EXISTS countries;
 
 -- Create booths table.
+
 CREATE TABLE booths (
     booth_no VARCHAR(5),
     type CHAR(2),
@@ -24,7 +25,8 @@ FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/booths.csv'  DELIMITER
 
 SELECT UpdateGeometrySRID('public', 'booths', 'geom', 4326);
 
--- Create investors table.
+-- Create exhibitors table.
+
 CREATE TABLE exhibitors (
     name VARCHAR,
     exchange VARCHAR,
@@ -37,7 +39,8 @@ CREATE TABLE exhibitors (
     CONSTRAINT investors_pkey PRIMARY KEY (name)
 );
 
--- Create trade show by booth relationship table.
+-- Create exhibitor-booth relationship table.
+
 CREATE TABLE exhibitor_by_booth (
     id SERIAL NOT NULL,
     name VARCHAR,
@@ -47,53 +50,57 @@ CREATE TABLE exhibitor_by_booth (
     FOREIGN KEY (booth_no) REFERENCES booths(booth_no)
 );
 
+-- Populate exhibitors table from IX, trade show, core shack.
 
 CREATE TEMP TABLE ex
 (LIKE exhibitors INCLUDING DEFAULTS)
-ON COMMIT DELETE ROWS;
+ON COMMIT PRESERVE ROWS;
 
 COPY ex(name, exchange, symbol, stock, add, country, geom, website)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ix.csv'  DELIMITER ',' CSV HEADER;
-INSERT INTO exhibitors SELECT * FROM ex ON CONFLICT DO NOTHING;
-
 COPY ex(name, exchange, symbol, stock, add, country, geom, website)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ts.csv'  DELIMITER ',' CSV HEADER;
-INSERT INTO exhibitors SELECT * FROM ex ON CONFLICT DO NOTHING;
-
 COPY ex(name, exchange, symbol, stock, add, country, geom, website)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_cs.csv'  DELIMITER ',' CSV HEADER;
-INSERT INTO exhibitors SELECT * FROM ex ON CONFLICT DO NOTHING;
+
+INSERT INTO exhibitors SELECT * FROM ex 
+ON CONFLICT DO NOTHING;
 
 DROP TABLE ex;
+
+-- Populate exhibitors table from Prospectors Tent.
 
 CREATE TEMPORARY TABLE prospectors (
     name VARCHAR(50),
     booth_no VARCHAR (5)
 )
-ON COMMIT DROP;
+ON COMMIT PRESERVE ROWS;
 
 COPY prospectors(booth_no, name)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/prospectors.csv'  DELIMITER ',' CSV HEADER;
-INSERT INTO exhibitors (name) SELECT name FROM prospectors
-ON CONFLICT DO NOTHING;
+INSERT INTO exhibitors (name) SELECT name FROM prospectors ON CONFLICT DO NOTHING;
 
-INSERT INTO exhibitor_by_booth(name, booth_no) SELECT name, booth_no FROM prospectors
-ON CONFLICT DO NOTHING;
+-- Update exhibitor CRS (necessary due to CSV import).
 
 SELECT UpdateGeometrySRID('public', 'exhibitors', 'geom', 4326);
 
--- Exhibitors by booth.
+-- Populate exhibitors-booth relationship tables from all categories.
 
 COPY exhibitor_by_booth(name, booth_no)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ix_by_booth.csv'  DELIMITER ',' CSV HEADER;
-
 COPY exhibitor_by_booth(name, booth_no)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ts_by_booth.csv'  DELIMITER ',' CSV HEADER;
-
 COPY exhibitor_by_booth(name, booth_no)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_cs_by_booth.csv'  DELIMITER ',' CSV HEADER;
+INSERT INTO exhibitor_by_booth(name, booth_no) SELECT name, booth_no FROM prospectors 
+ON CONFLICT DO NOTHING;
 
--- Create trade show by biztype relationship table.
+-- Done with prospectors temp table.
+
+DROP TABLE prospectors;
+
+-- Create trade show-biztype relationship table.
+
 CREATE TABLE ts_by_biztype (
     id SERIAL NOT NULL,
     name VARCHAR,
@@ -102,10 +109,13 @@ CREATE TABLE ts_by_biztype (
     FOREIGN KEY (name) REFERENCES exhibitors(name)
 );
 
+-- Populate trade show-biztype relationship table.
+
 COPY ts_by_biztype(name, biztype)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ts_by_biztype.csv'  DELIMITER ',' CSV HEADER;
 
 -- Create countries table.
+
 CREATE TABLE countries (
     country VARCHAR,
     country_long VARCHAR,
@@ -115,12 +125,17 @@ CREATE TABLE countries (
     CONSTRAINT countries_pkey PRIMARY KEY (country_long)
 );
 
+-- Populate countries table with Natural Earth data (csv-ed).
+
 COPY countries(country, country_long, iso_a2, iso_a3, geom)
-FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/countries_poly.csv'  DELIMITER ',' CSV HEADER;
+FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/countries.csv'  DELIMITER ',' CSV HEADER;
+
+-- Update countries CRS (necessary due to CSV import).
 
 SELECT UpdateGeometrySRID('public', 'countries', 'geom', 4326);
 
--- Create investors by commodity relationship table.
+-- Create investor-commodity relationship table.
+
 CREATE TABLE ix_by_commodity (
     id SERIAL NOT NULL,
     name VARCHAR,
@@ -129,10 +144,13 @@ CREATE TABLE ix_by_commodity (
     FOREIGN KEY (name) REFERENCES exhibitors(name)
 );
 
+-- Populate investor-commodity relationship table.
+
 COPY ix_by_commodity(name, commodity)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ix_by_commodity.csv'  DELIMITER ',' CSV HEADER;
 
--- Create investors by country relationship table.
+-- Create investor-country relationship table.
+
 CREATE TABLE ix_by_country (
     id SERIAL NOT NULL,
     name VARCHAR,
@@ -142,10 +160,13 @@ CREATE TABLE ix_by_country (
     FOREIGN KEY (country) REFERENCES countries(country_long)
 );
 
+-- Populate investor-country relationship table.
+
 COPY ix_by_country(name, country)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_ix_by_country.csv'  DELIMITER ',' CSV HEADER;
 
--- Create core shack by project table.
+-- Create core shack-project relationship table.
+
 CREATE TABLE cs_by_projects (
     proj VARCHAR,
     loc VARCHAR,
@@ -156,22 +177,11 @@ CREATE TABLE cs_by_projects (
     FOREIGN KEY (name) REFERENCES exhibitors(name)
 );
 
+-- Populate core shack-project relationship table.
+
 COPY cs_by_projects(proj, loc, country, name, geom)
 FROM '/Users/ehuntley/Desktop/data/beyond-extraction/data/firms_cs_by_projects.csv'  DELIMITER ',' CSV HEADER;
 
-SELECT UpdateGeometrySRID('public', 'cs_by_projects', 'geom', 4326);
+-- Update CS project CRS (necessary due to CSV import).
 
-CREATE VIEW exhibitor_addresses AS
-SELECT
-    e.name, 
-    MAX(e.add) AS add, 
-    MAX(e.country) AS country, 
-    STRING_AGG(b.type, ', ') AS types, 
-    STRING_AGG(eb.booth_no, ', ') AS booths
-FROM exhibitors e
-    LEFT JOIN exhibitor_by_booth eb 
-    ON e.name = eb.name
-        LEFT JOIN booths b 
-        ON eb.booth_no=b.booth_no
-GROUP BY e.name
-ORDER BY types;
+SELECT UpdateGeometrySRID('public', 'cs_by_projects', 'geom', 4326);
